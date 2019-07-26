3c195a9d5afcfef0062658451d0269f56b8e861b
Borys Krisko <bvkr@me.com>
2019-07-26 14:11:14 +0300

    Initial Commit

---
+++ HW2-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW2-2
+//
+//  Created by Борис Крисько on 7/26/19.
+//  Copyright © 2019 Borys Krisko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
