1049e63fa3085075c449df983a2a9060c538ab6c
Borys Krisko <bvkr@me.com>
2019-07-26 14:44:41 +0300

    first version

--- HW2-2/ViewController.swift
+++ HW2-2/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewArea: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func redSliderChanged(_ sender: Any) {
+        viewArea.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+        redTextField.text = String(redSlider.value)
+    }
+    @IBAction func greenSliderChanged(_ sender: Any) {
+    }
+    @IBAction func blueSliderChanged(_ sender: Any) {
+    }
 }
 
