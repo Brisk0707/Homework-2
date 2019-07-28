cf1f2dcb3346c00832768ba8095f6ce9eb5bf9b2
Borys Krisko <bvkr@me.com>
2019-07-28 20:16:32 +0300

    almost finish

--- HW2-2/ViewController.swift
+++ HW2-2/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
+    
     @IBOutlet weak var viewArea: UIView!
     
     @IBOutlet weak var redSlider: UISlider!
@@ -28,81 +28,81 @@ class ViewController: UIViewController, UITextFieldDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self,
+                                                                 action: #selector(UIInputViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
-
-
+        
         let toolbar = UIToolbar()
         toolbar.sizeToFit()
         
-        let flexiblespace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        let flexiblespace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                            target: nil,
+                                            action: nil)
         
-        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.doneClicked))
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done,
+                                         target: self,
+                                         action: #selector(self.doneClicked))
         
         toolbar.setItems([flexiblespace, doneButton], animated: true)
         
         redTextField.inputAccessoryView = toolbar
         greenTextField.inputAccessoryView = toolbar
         blueTextField.inputAccessoryView = toolbar
-        
-        
+    }
     
+    @IBAction func redSliderChanged(_ sender: Any) {
+        actionsWhenSliderChanged()
+    }
+    @IBAction func greenSliderChanged(_ sender: Any) {
+        actionsWhenSliderChanged()
+        
+    }
+    @IBAction func blueSliderChanged(_ sender: Any) {
+        actionsWhenSliderChanged()
     }
     
     @objc func dismissKeyboard() {
-        redSlider.value = Float(redTextField.text!)!
-        greenSlider.value = Float(greenTextField.text!)!
-        blueSlider.value = Float(blueTextField.text!)!
-        
-        
-        viewArea.backgroundColor = UIColor(red: CGFloat(Float(redTextField.text!)!/255), green: CGFloat(Float(greenTextField.text!)!/255), blue: CGFloat(Float(blueTextField.text!)!/255), alpha: 1)
-        
-        redLabel.text = redTextField.text!
-        blueLabel.text = blueTextField.text!
-        greenLabel.text = greenTextField.text!
+        actionsWhenInputFromKeyboardOrTap()
         view.endEditing(true)
     }
-    
-    
     
     @objc func doneClicked() {
+        actionsWhenInputFromKeyboardOrTap()
         view.endEditing(true)
-        
-        
-        redSlider.value = Float(redTextField.text!)!
-        greenSlider.value = Float(greenTextField.text!)!
-        blueSlider.value = Float(blueTextField.text!)!
-        
-        
-        viewArea.backgroundColor = UIColor(red: CGFloat(Float(redTextField.text!)!/255), green: CGFloat(Float(greenTextField.text!)!/255), blue: CGFloat(Float(blueTextField.text!)!/255), alpha: 1)
-        
-        redLabel.text = redTextField.text!
-        blueLabel.text = blueTextField.text!
-        greenLabel.text = greenTextField.text!
-        
     }
     
     
-    
-    
-    
-    
-
-    @IBAction func redSliderChanged(_ sender: Any) {
-        viewArea.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value / 255), alpha: 1)
-        redLabel.text = String(round(redSlider.value*100)/100)
-        redTextField.text = String(round(redSlider.value*100)/100)
+    func actionsWhenSliderChanged(){
+        viewArea.backgroundColor = UIColor(red: CGFloat(redSlider.value / 255),
+                                           green: CGFloat(greenSlider.value / 255),
+                                           blue: CGFloat(blueSlider.value / 255),
+                                           alpha: 1)
+        
+        redLabel.text = String(round(redSlider.value * 100) / 100)
+        redTextField.text = String(round(redSlider.value * 100) / 100)
+        
+        greenLabel.text = String(round(greenSlider.value * 100) / 100)
+        greenTextField.text = String(round(greenSlider.value * 100) / 100)
+        
+        blueLabel.text = String(round(blueSlider.value * 100) / 100)
+        blueTextField.text = String(round(blueSlider.value * 100) / 100)
     }
-    @IBAction func greenSliderChanged(_ sender: Any) {
-        viewArea.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value / 255), alpha: 1)
-        greenLabel.text = String(round(greenSlider.value*100)/100)
-        greenTextField.text = String(round(greenSlider.value*100)/100)
-
-    }
-    @IBAction func blueSliderChanged(_ sender: Any) {
-        viewArea.backgroundColor = UIColor(red: CGFloat(redSlider.value/255), green: CGFloat(greenSlider.value/255), blue: CGFloat(blueSlider.value/255), alpha: 1)
-        blueLabel.text = String(round(blueSlider.value*100)/100)
-        blueTextField.text = String(round(blueSlider.value*100)/100)
+    
+    func actionsWhenInputFromKeyboardOrTap() {
+        
+        if redTextField.text != nil && greenTextField.text != nil && blueTextField.text != nil {
+            redSlider.value = Float(redTextField.text!)!
+            greenSlider.value = Float(greenTextField.text!)!
+            blueSlider.value = Float(blueTextField.text!)!
+            
+            viewArea.backgroundColor = UIColor(red: CGFloat(Float(redTextField.text!)! / 255),
+                                               green: CGFloat(Float(greenTextField.text!)! / 255),
+                                               blue: CGFloat(Float(blueTextField.text!)! / 255),
+                                               alpha: 1)
+            
+            redLabel.text = redTextField.text
+            blueLabel.text = blueTextField.text!
+            greenLabel.text = greenTextField.text!
+        }
     }
 }
-
